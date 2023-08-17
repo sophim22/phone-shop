07ceef8538e26d9ff7b37fdc10ecda6a94595323
sophim22 <sophimkonmak198@gmail.com>
2023-08-17 12:12:00 +0700

    fix-translate

--- next.config.js
+++ next.config.js
@@ -2,7 +2,11 @@ const { i18n } = require("./next-i18next.config.js");
 require('dotenv').config();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  i18n,
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'km'],
+  },
+  localePath: path.resolve('./public/locales'),
   reactStrictMode: true,
   env: {
     BASE_URL: process.env.BASE_URL,
